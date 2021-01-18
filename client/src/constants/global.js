2eeb927808bb748777f44f1c212c0e4fcc6b6c03
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-01-18 16:18:28 +0700

    up first

---
+++ client/src/constants/global.js
@@ -0,0 +1,7 @@
+export const STRING = {
+    LOGIN:'Đăng nhập',
+    SINGUP: 'Đăng ký'
+    ERROR_MESSAGES: {
+        EMAIL_MISSING: 'Bạn chưa nhập Email'
+    }
+}
---
+++ server/src/data/post.data.js
@@ -0,0 +1,13 @@
+[
+    {
+        content:'hihi',
+        reaction: {
+            like:3,
+            dislike:69,
+            smile:22,
+            heart:3
+        },
+        img:'img/author.jpg',
+        author: 'Hoàng Vũ'
+    },
+]
